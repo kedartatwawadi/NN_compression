3e90c8feb7b70fad570873fa413b8d330585d326
kedartatwawadi <ktatwawadi@gmail.com>
2017-03-20 00:26:48 -0700

    adding wiki running model

---
+++ tf_char_rnn/char_feature_rnn_wiki.py
@@ -0,0 +1,58 @@
+""" Based partially on CS224n assignment 3
+"""
+from __future__ import print_function
+import os
+import time
+import argparse
+import tensorflow as tf
+import numpy as np
+from sequence_pred_feature_model import SequencePredFeature
+from feature_model_trainer import ModelTrainer
+
+def get_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default='../data/enwiki9_text',
+                        help='data directory containing input.txt')
+    parser.add_argument('--info_path', type=str, default=None,
+                       help='Information about the input file')
+    parser.add_argument('--summary_path', type=str, default='.summary_wiki/features_64',
+                       help='directory to store tf summary')
+    parser.add_argument('--hidden_size', type=int, default=128,
+                       help='size of RNN hidden state')
+    parser.add_argument('--num_layers', type=int, default=2,
+                       help='number of layers in the RNN')
+    parser.add_argument('--batch_size', type=int, default=64,
+                       help='minibatch size')
+    parser.add_argument('--feature_size', type=int, default=64,
+                       help='feature size')
+    parser.add_argument('--num_epochs', type=int, default=10,
+                       help='number of epochs')
+    parser.add_argument('--lr', type=float, default=0.0002,
+                       help='learning rate')
+    parser.add_argument('--decay_rate', type=float, default=0.97,
+                       help='decay rate for the training')
+    parser.add_argument('--model_type', type=str, default="gru")
+    parser.add_argument('--vocab', type=str, default="abcdefghijklmnopqrstuvwxyz ")
+    parser.add_argument('--entropy', type=float, default=0)
+    parser.add_argument('--dropout', type=float, default=1.0)
+    parser.add_argument('--print_every', type=int, default=10)
+    parser.add_argument('--validate_every', type=int, default=100000000000000000)
+    parser.add_argument('--validate_path', type=str, default='data/sequence_data/validate.txt')                     
+    
+    return parser
+
+def get_config_args():
+    parser = get_argument_parser()
+    config = parser.parse_args()
+    config.max_length = config.batch_size
+    config.num_classes = len(config.vocab)
+    return config
+
+def main():
+    config = get_config_args()
+    GRUModel = SequencePredFeature(config);
+    Trainer = ModelTrainer(config, GRUModel)
+    Trainer.do_training();
+    
+if __name__ == '__main__':
+    main()
